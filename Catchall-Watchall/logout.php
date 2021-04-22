6fc3ec96d98eb32eab8e74c262aceb2dd54951d3
kallen52 <kallen52@kent.edu>
2021-04-22 19:49:47 -0400

    Login and logout

---
+++ Catchall-Watchall/logout.php
@@ -0,0 +1,10 @@
+<?php echo file_get_contents("header.html"); ?>
+
+<html>
+    <form action="logoutprocess.php">
+    <div class="jumbotron">
+        <p>Are you sure you want to log out?</p>
+        <p><input class="btn btn-primary btn-large" type="submit" value="Yes">  <a href="http://localhost/Catchall-Watchall/index.php" class="btn btn-primary btn-large">No</a></p>
+    </div>
+    </form>
+<html>
