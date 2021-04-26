d9a24dbc4d115d427153c0ea8628c0288d64eb5d
kallen52 <kallen52@kent.edu>
2021-04-26 14:56:41 -0400

    Final draft of the website

--- Catchall-Watchall/login.php
+++ Catchall-Watchall/login.php
@@ -1,12 +1,7 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" 
 
-"http://www.w3.org/TR/html4/loose.dtd">
+<?php echo file_get_contents("body.html"); ?>
 
-<html>
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
-<title>Login</title>
-</head>
+
 <body>
 
 	<div id="frm">
@@ -34,5 +29,11 @@
 			</center>
     </form>
 	</div>
+  <div>
+  <br>
+  <center>
+    <p><a href="http://localhost/Catchall-Watchall/register.php" class="btn btn-primary btn-large">Create an Account</a></p>
+  </center>
+  </div>
 </body>
 </html>
