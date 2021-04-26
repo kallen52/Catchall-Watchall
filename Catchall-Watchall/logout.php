8449e2633f17f0da51e718750e2de6a8ac2aeb23
kallen52 <kallen52@kent.edu>
2021-04-25 20:25:20 -0400

    Watchlist entries can now be deleted

--- Catchall-Watchall/logout.php
+++ Catchall-Watchall/logout.php
@@ -5,7 +5,7 @@
 <html>
     <form action="logoutprocess.php">
     <div class="jumbotron">
-        <p>Are you sure you want to log out?</p>
+        <h3>Are you sure you want to log out?</h3>
         <p><input class="btn btn-primary btn-large" type="submit" value="Yes">  <a href="http://localhost/Catchall-Watchall/index.php" class="btn btn-primary btn-large">No</a></p>
     </div>
     </form>
